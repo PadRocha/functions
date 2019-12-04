3d1ed7b67da76bebf056364f12cee1240c1f274c
Joshua Elí Padrón Rocha <p.roch@outlook.es>
2019-12-04 09:32:53 -0600

    Inicio de unciones

---
+++ funtions.js
@@ -0,0 +1,74 @@
+function randomProbability(min, max, matrizNumeros, matrizProbabilidad) {
+    for (let i = 0; i < matrizProbabilidad.length; i++)
+      if (Math.random() < matrizProbabilidad[i]) return matrizNumeros[i];
+    return Math.floor(Math.random() * (max - min + 1) + min);
+  }
+  
+function random(minimo,maximo) { return Math.floor(Math.random() * (maximo - minimo + 1) + minimo); }
+
+function syntaxHighlight(json) {
+    json = json.replace(/&/g, '&amp;').replace(/</g, '&lt;').replace(/>/g, '&gt;');
+    return json.replace(/("(\\u[a-zA-Z0-9]{4}|\\[^u]|[^\\"])*"(\s*:)?|\b(true|false|null)\b|-?\d+(?:\.\d*)?(?:[eE][+\-]?\d+)?)/g, function (match) {
+        var cls = 'number';
+        if (/^"/.test(match)) {
+            if (/:$/.test(match)) {
+                cls = 'key';
+            } else {
+                cls = 'string';
+            }
+        } else if (/true|false/.test(match)) {
+            cls = 'boolean';
+        } else if (/null/.test(match)) {
+            cls = 'null';
+        }
+        return '<span class="' + cls + '">' + match + '</span>';
+    });
+}
+
+function addEvent(e, n, f) {return e.attachEvent?e.attachEvent('on'+n,f):e.addEventListener(n, f, !!0)}
+
+function postAjax(url, data, success) {
+    var params = typeof data == 'string' ? data : Object.keys(data).map(
+        function (k) {
+            return encodeURIComponent(k) + '=' + encodeURIComponent(data[k])
+        }
+    ).join('&');
+
+    var xhr = window.XMLHttpRequest ? new XMLHttpRequest() : new ActiveXObject('Microsoft.XMLHTTP');
+    xhr.open('POST', url);
+    xhr.onreadystatechange = e => {
+        if (xhr.readyState > 3 && xhr.status == 200) {
+            success(xhr.responseText);
+        }
+    };
+    xhr.setRequestHeader('X-Requested-With', 'XMLHttpRequest');
+    xhr.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
+    xhr.send(params);
+    return xhr;
+}
+
+function getUrlVars() {
+    var vars = {};
+    var parts = window.location.href.replace(/[?&]+([^=&]+)=([^&]*)/gi, function (m, key, value) {
+        vars[key] = value;
+    });
+    return vars;
+}
+
+function getUrlParam(parameter, defaultvalue) {
+    var urlparameter = defaultvalue;
+    if (window.location.href.indexOf(parameter) > -1) {
+        urlparameter = getUrlVars()[parameter];
+    }
+    return urlparameter;
+}
+
+function clearElement(e) {
+    Array.from(e.childNodes).forEach(child => {
+        e.removeChild(child);
+    });
+}
+
+function output(inp) {
+    document.body.appendChild(document.createElement('pre')).innerHTML = inp;
+}
