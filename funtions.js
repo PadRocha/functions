a8a78c78ad8b6015a7ab06759c55dc66df6877ab
Joshua Elí Padrón Rocha <p.roch@outlook.es>
2019-12-08 16:41:31 -0600

    update

--- funtions.js
+++ funtions.js
@@ -27,10 +27,6 @@ function syntaxHighlight(json) {
     });
 }
 
-// function addEvent(e, n, f) {
-//     return e.attachEvent ? e.attachEvent('on' + n, f) : e.addEventListener(n, f, !!0)
-// }
-
 function getAjax(url, success) {
     var xhr = window.XMLHttpRequest ? new XMLHttpRequest() : new ActiveXObject('Microsoft.XMLHTTP');
     xhr.open('GET', url);
@@ -119,6 +115,19 @@ function add_property(obj, key, val) {
     obj[key[0]] = val;
 }
 
+function clone(obj) {
+    if (obj === null || typeof obj !== 'object') {
+        return obj;
+    }
+
+    var temp = obj.constructor();
+    for (var key in obj) {
+        temp[key] = clone(obj[key]);
+    }
+
+    return temp;
+}
+
 String.prototype.capitalize = function (str) {
     if (!str) {
         str = this;
@@ -164,6 +173,91 @@ String.prototype.allReplace = function (obj) {
 // document.getSelection();
 // document.hasChildNodes();
 // document.hasFocus();
+
+const color = {
+    pSBC: (p, c0, c1, l) => {
+        //https://github.com/PimpTrizkit/PJs/wiki/12.-Shade,-Blend-and-Convert-a-Web-Color-(pSBC.js) // Version 4.0
+        let r, g, b, P, f, t, h, i = parseInt,
+            m = Math.round,
+            a = typeof (c1) == "string";
+        if (typeof (p) != "number" || p < -1 || p > 1 || typeof (c0) != "string" || (c0[0] != 'r' && c0[0] != '#') || (c1 && !a)) return null;
+        if (!this.pSBCr) this.pSBCr = (d) => {
+            let n = d.length,
+                x = {};
+            if (n > 9) {
+                [r, g, b, a] = d = d.split(","), n = d.length;
+                if (n < 3 || n > 4) return null;
+                x.r = i(r[3] == "a" ? r.slice(5) : r.slice(4)), x.g = i(g), x.b = i(b), x.a = a ? parseFloat(a) : -1
+            } else {
+                if (n == 8 || n == 6 || n < 4) return null;
+                if (n < 6) d = "#" + d[1] + d[1] + d[2] + d[2] + d[3] + d[3] + (n > 4 ? d[4] + d[4] : "");
+                d = i(d.slice(1), 16);
+                if (n == 9 || n == 5) x.r = d >> 24 & 255, x.g = d >> 16 & 255, x.b = d >> 8 & 255, x.a = m((d & 255) / 0.255) / 1000;
+                else x.r = d >> 16, x.g = d >> 8 & 255, x.b = d & 255, x.a = -1
+            }
+            return x
+        };
+        h = c0.length > 9, h = a ? c1.length > 9 ? true : c1 == "c" ? !h : false : h, f = this.pSBCr(c0), P = p < 0, t = c1 && c1 != "c" ? this.pSBCr(c1) : P ? {
+            r: 0,
+            g: 0,
+            b: 0,
+            a: -1
+        } : {
+            r: 255,
+            g: 255,
+            b: 255,
+            a: -1
+        }, p = P ? p * -1 : p, P = 1 - p;
+        if (!f || !t) return null;
+        if (l) r = m(P * f.r + p * t.r), g = m(P * f.g + p * t.g), b = m(P * f.b + p * t.b);
+        else r = m((P * f.r ** 2 + p * t.r ** 2) ** 0.5), g = m((P * f.g ** 2 + p * t.g ** 2) ** 0.5), b = m((P * f.b ** 2 + p * t.b ** 2) ** 0.5);
+        a = f.a, t = t.a, f = a >= 0 || t >= 0, a = f ? a < 0 ? t : t < 0 ? a : a * P + t * p : 0;
+        if (h) return "rgb" + (f ? "a(" : "(") + r + "," + g + "," + b + (f ? "," + m(a * 1000) / 1000 : "") + ")";
+        else return "#" + (4294967296 + r * 16777216 + g * 65536 + b * 256 + (f ? m(a * 255) : 0)).toString(16).slice(1, f ? undefined : -2)
+    },
+    hex: () => {
+        var letters = '0123456789ABCDEF';
+        var color = '#';
+        for (var i = 0; i < 6; i++) {
+            color += letters[Math.floor(Math.random() * 16)];
+        }
+        return color;
+    },
+    rgb: () => {
+        return 'rgb(' +
+            Math.floor(Math.random() * 256) + ', ' +
+            Math.floor(Math.random() * 256) + ', ' +
+            Math.floor(Math.random() * 256) + ')';
+    },
+    rgba: () => {
+        return 'rgb(' +
+            Math.floor(Math.random() * 256) + ', ' +
+            Math.floor(Math.random() * 256) + ', ' +
+            Math.floor(Math.random() * 256) + ', ' +
+            Math.random().toFixed(2) + ')';
+    },
+    hsl: () => {
+        return 'hsl(' +
+            Math.floor(Math.random() * 361) + ',' +
+            Math.floor(Math.random() * 101) + '%,' +
+            Math.floor(Math.random() * 101) + '%)';
+    },
+    hsla: () => {
+        return 'hsl(' +
+            Math.floor(Math.random() * 361) + ', ' +
+            Math.floor(Math.random() * 101) + '%, ' +
+            Math.floor(Math.random() * 101) + '%, ' +
+            Math.random().toFixed(2) + ')';
+    },
+    toHex: (str, type = null) => {
+        if (type !== null) {
+            str = type + '(' + str.join(', ') + ')';
+        }
+        var ctx = document.createElement("canvas").getContext("2d");
+        ctx.fillStyle = str;
+        return ctx.fillStyle;
+    }
+}
 
 const j = {
     // Selecciona un Elemento (Solo el primero)
@@ -268,6 +362,37 @@ const j = {
     $clone: (e, set = document) => {
         return (typeof e === 'string' || e instanceof String ? set.querySelector(e) : e).cloneNode(true);
     },
+    $create: (e, attribute = null, insert = null, append = null, set = document, ns = null) => {
+        if (ns === null) {
+            var element = document.createElement(e);
+        } else {
+            var element = document.createElementNS(ns, e);
+        }
+        if (typeof attribute == 'object' || attribute instanceof Object && element) {
+            for (let property in attribute)
+                element.setAttribute(property, attribute[property]);
+        }
+        if (typeof insert === 'string' || insert instanceof String) {
+            element.appendChild(document.createTextNode(insert));
+        } else if (insert !== null && insert.nodeType === 1) {
+            element.appendChild(insert);
+        } else if (Array.isArray(insert)) {
+            insert.forEach(e => {
+                if (typeof e === 'string' || e instanceof String) {
+                    element.appendChild(document.createTextNode(e));
+                } else if (typeof e === 'object' || e instanceof Object) {
+                    element.appendChild(e);
+                }
+            });
+        }
+        if (typeof append === 'string' || append instanceof String) {
+            set.querySelector(append).appendChild(element);
+        } else if (insert !== null && append.nodeType === 1) {
+            append.appendChild(element);
+        } else {
+            return element;
+        }
+    },
     $addEvent: (e, n, f, set = document) => {
         let element = (typeof e === 'string' || e instanceof String ? set.querySelector(e) : e);
         if (element) {
@@ -293,11 +418,11 @@ const j = {
     $$hide: (e, hide = true, set = document) => {
         let elements = (typeof e === 'string' || e instanceof String ? set.querySelectorAll(e) : e);
         if (hide && elements) {
-            elements.forEach(() => {
+            elements.forEach(e => {
                 e.style.display = 'none';
             });
         } else if (elements) {
-            elements.forEach(() => {
+            elements.forEach(e => {
                 e.style.display = 'block';
             });
         }
